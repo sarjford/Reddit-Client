703c72a2c740ad92088555d81305ca679d961b3a
Sarah <sarah.jeong.ford@gmail.com>
2020-04-03 10:16:50 -0700

    Remove un-needed files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sarah Ford Reddit Client`,
+    description: `Reddit Client SPA`,
+    author: `@sarjford`,
   },
   plugins: [
     {
@@ -18,17 +18,6 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-sharp`
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
     },
     {
       resolve: `gatsby-plugin-layout`,
